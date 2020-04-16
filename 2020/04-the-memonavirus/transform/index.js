7bfb5d6849892bdabd8b4d9e9815fa19f5c2f2a4
Mehdi El Fadil <m.ef@mango-is.com>
2020-04-16 11:58:13 +0200

    add generation of infection curve timeline
    + refactor transform for better manageability of transformation steps

--- 2020/04-the-memonavirus/transform/index.js
+++ 2020/04-the-memonavirus/transform/index.js
@@ -5,10 +5,149 @@ let commentFiles = []
 	, infectionFiles = []
 	, filePath
 	, graph = new DirectedGraph()
+	, transformSteps
+	, currentTransformStep = 0
+	, res = {}
 
-function listFiles(path, callback) {
+
+/****************************
+ *
+ * Add infection track to the graph based on logFile contents
+ *
+ * pre-requisite: comment graph is populated
+ *
+ *****************************/
+function addInfections(currentIndex) {
 	
-	fs.readdir(path, function(err, files) {
+	if (!currentIndex)
+		currentIndex = 0
+	
+	if (currentIndex === infectionFiles.length) {
+	//~if (currentIndex === 2) {
+
+		console.log('... All infection log files processed')
+		console.log(graph.toString())
+
+
+		//~console.log(JSON.stringify(graph.toJSON(), null, '    '))
+		
+		//~console.log('Save graph export file...')
+		//~fs.writeFile(__dirname + '/../data/graphExport.json', JSON.stringify(graph.toJSON()), function(err, res) {
+			//~if (err)
+				//~throw err
+			
+			//~console.log('graphExport.json saved')
+		//~})
+
+		next()
+	}
+	else  {
+
+		fs.readFile(filePath + '/' + infectionFiles[currentIndex], 'utf8', function(err, content) {
+			if (err)
+				throw err
+				
+			let logFile = content.split('\n')
+			
+			// remove the last empty line of the file
+			logFile.pop()
+			
+			logFile.forEach( function(logLine) {
+				
+				let data = logLine.split('\t')
+				
+				try {
+					
+					// record infected node
+					graph.mergeNodeAttributes(data[1], {
+						infection_ts: data[0]
+					})
+					
+					// record infectious edge
+					graph.mergeEdgeAttributes(data[1], data[3], {
+						infection_ts: data[0] // infection timestamp
+					})
+					
+				}
+				catch(e) {
+					// The infection log line for patient 0 does not have a parent, hence graph.mergeEdgeAttributes fails to run.
+					
+					console.log('patient 0 is: ' + data[1] + ' - infected on ' + data[0])
+					
+					// record infected node
+					graph.mergeNodeAttributes(data[1], {
+						infection_ts: data[0]
+					})
+				}
+				
+			})
+
+			addInfections(++currentIndex)
+			
+		})
+		
+	}
+	
+}
+
+
+/****************************
+ *
+ * Build the infections curve
+ * 
+ * In order to measure the activity (number of comments) per hour, it is necessary to iterate through the edges.
+ * 
+ * Should it be needed only to get the infections, iterating though the graph's nodes would be more efficient.
+ * 
+ *****************************/
+function setCurve() {
+
+	let figuresPerHour = {}
+	
+	graph.forEachEdge(function(edge, attributes) {
+		
+		let commentHour = getHourTs(attributes.ts)
+		
+		if (!figuresPerHour[commentHour])
+			figuresPerHour[commentHour] = {
+				comments: 0
+				, infections: 0
+			}
+		
+		figuresPerHour[commentHour].comments++
+		
+		if (attributes.infection_ts) {
+			let infectionHour = getHourTs(attributes.infection_ts)
+			
+			if (!figuresPerHour[infectionHour])
+				figuresPerHour[infectionHour] = {
+					comments: 0
+					, infections: 0
+				}
+				
+			figuresPerHour[infectionHour].infections++
+		}
+		
+	})
+	
+	// temp output
+	
+	let sortedTimestamps = Object.keys(figuresPerHour).map(ts => parseInt(ts)).sort((a, b) => a - b)
+	
+	sortedTimestamps.forEach(ts => console.log(new Date(ts), figuresPerHour[ts].comments, figuresPerHour[ts].infections))
+	
+	
+	
+}
+
+/****************************
+ *
+ * Lookup data source location 
+ *
+ *****************************/
+function listFiles() {
+	
+	fs.readdir(filePath, function(err, files) {
 		if (err)
 			throw err
 		else {
@@ -29,20 +168,31 @@ function listFiles(path, callback) {
 			console.log(commentFiles.length + ' comment files')
 			console.log(infectionFiles.length + ' infection files')
 
-		console.log('Start processing comment log files...')			
-			callback()
+			console.log('Start processing comment log files...')			
+			next()
 		}
 	})
 	
 }
 
-function processComments(currentIndex, callback) {
+
+/****************************
+ *
+ * Build comment graph based on logFile contents
+ *
+ * pre-requisite: comment graph is populated
+ *
+ *****************************/
+function setCommentGraph(currentIndex) {
+	
+	if (!currentIndex)
+		currentIndex = 0
 	
 	if (currentIndex === commentFiles.length) {
 	//~if (currentIndex === 1) {
 		console.log('... All comment log files processed')
 		console.log('Start processing infection log files...')
-		callback(0)
+		next()
 	}
 	else  {
 
@@ -70,7 +220,7 @@ function processComments(currentIndex, callback) {
 				
 			})
 			
-			processComments(++currentIndex, callback)
+			setCommentGraph(++currentIndex)
 			
 		})
 
@@ -79,70 +229,44 @@ function processComments(currentIndex, callback) {
 }
 
 
-function processInfections(currentIndex) {
+
+/****************************
+ *
+ * Launch next transformation step
+ *
+ *****************************/
+function next() {
 	
-	if (currentIndex === infectionFiles.length) {
-	//~if (currentIndex === 2) {
-
-		console.log('... All infection log files processed')
-		console.log('Save graph export file...')
-
-		console.log(graph.toString())
-		//~console.log(JSON.stringify(graph.toJSON(), null, '    '))
+	if (currentTransformStep === transformSteps.length) {
+		console.log('all transformations performed')
+	}
+	else
+		transformSteps[currentTransformStep++]()
 		
-		fs.writeFile(__dirname + '/../data/graphExport.json', JSON.stringify(graph.toJSON()), function(err, res) {
-			if (err)
-				throw err
-			
-			console.log('graphExport.json saved')
-		})
-	}
-	else  {
-
-		fs.readFile(filePath + '/' + infectionFiles[currentIndex], 'utf8', function(err, content) {
-			if (err)
-				throw err
-				
-			let logFile = content.split('\n')
-			
-			// remove the last empty line of the file
-			logFile.pop()
-			
-			logFile.forEach( function(logLine) {
-				
-				let data = logLine.split('\t')
-				
-				try {
-					graph.mergeEdgeAttributes(data[1], data[3], {
-						i: true
-						, i_ts: data[0] // infection timestamp
-					})
-				}
-				catch(e) {
-					graph.mergeEdge(data[1], data[3], {
-						ts: data[0]
-						, type: data[5] // C = commented on comment, S = commented on submission
-						, i: true
-						, i_ts: data[0] // infection timestamp
-					})
-				}
-				
-			})
-
-			processInfections(++currentIndex)
-			
-		})
-		
-	}
-	
 }
 
-function generateGraph() {
+/****************************
+ *
+ * Utility function to convert a timestamp string to a date, truncated to the hour.
+ * 
+ * @param ts {string} timestamp string as formatted in reddit logs
+ *
+ *****************************/
+function getHourTs(ts) {
 	
-	processComments(0, processInfections)
+	// temp
+	//~return ts.substr(0,13)
 	
+	ts = new Date(ts)
+	
+	return new Date(ts.setMinutes(0, 0, 0)).valueOf()
 }
 
+/****************************
+ *
+ * Start processing
+ *
+ *****************************/
 fs.readFile(__dirname + '/../data/metadata', 'utf8', function(err, path) {
 	if (err)
 		throw err
@@ -153,5 +277,8 @@ fs.readFile(__dirname + '/../data/metadata', 'utf8', function(err, path) {
 	console.log('-----------------------------------------------------')	
 	console.log('Starting data transformation...', filePath)
 	
-	listFiles(filePath, generateGraph)
+	transformSteps = [listFiles, setCommentGraph, addInfections, setCurve]
+	
+	// launch first step
+	next()
 })
